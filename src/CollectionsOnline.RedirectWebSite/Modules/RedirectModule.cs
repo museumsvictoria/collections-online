2e6048f89052e1f57edfec3fd7a4bdd861812885
michael-mason <cptshiner@gmail.com>
2013-11-19 16:08:30 +1100

    added redirect website

---
+++ src/CollectionsOnline.RedirectWebSite/Modules/RedirectModule.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using Nancy;
+using Nancy.Responses;
+
+namespace CollectionsOnline.RedirectWebSite.Modules
+{
+    public class RedirectModule : NancyModule
+    {
+        public RedirectModule()
+        {
+            Get["/"] = parameters => new RedirectResponse(ConfigurationManager.AppSettings["WebsiteUrl"], RedirectResponse.RedirectType.Permanent);
+        }
+    }
+}
