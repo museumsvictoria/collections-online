617c62836efb34a9e2bce2410b582a91eee019e7
michael-mason <cptshiner@gmail.com>
2015-08-03 14:01:25 +1000

    added redirect module
    - added redirect module to permanently re-route requests from the old
    natural science collections site

---
+++ src/CollectionsOnline.WebSite/Modules/RedirectModule.cs
@@ -0,0 +1,23 @@
+﻿using Nancy;
+using Nancy.Responses;
+
+namespace CollectionsOnline.WebSite.Modules
+{
+    public class RedirectModule : NancyModule
+    {
+        public RedirectModule()
+        {
+            Get["/search.php"] = parameters => new RedirectResponse("/search", RedirectResponse.RedirectType.Permanent);
+
+            Get["/browser.php"] = parameters => new RedirectResponse("/search", RedirectResponse.RedirectType.Permanent);            
+
+            Get["/results.php"] = parameters => new RedirectResponse("/search", RedirectResponse.RedirectType.Permanent);
+
+            Get["/resultsns.php"] = parameters => new RedirectResponse("/search", RedirectResponse.RedirectType.Permanent);
+
+            Get["/object.php"] = parameters => new RedirectResponse("/", RedirectResponse.RedirectType.Permanent);
+
+            Get["/imagedisplay.php"] = parameters => new RedirectResponse("/", RedirectResponse.RedirectType.Permanent);
+        }
+    }
+}
