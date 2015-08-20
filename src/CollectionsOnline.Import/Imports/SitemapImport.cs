a764ec86c884cac984b28aca0ef05572eb5606d7
michael-mason <cptshiner@gmail.com>
2015-08-20 11:32:30 +1000

    fix sitemap import
    - fixed missing extension on sitemap url sets contained within the
    sitemap index

--- src/CollectionsOnline.Import/Imports/SitemapImport.cs
+++ src/CollectionsOnline.Import/Imports/SitemapImport.cs
@@ -96,7 +96,7 @@ namespace CollectionsOnline.Import.Imports
                 count++;
 
                 sitemapIndex.Add(new XElement(xmlns + "sitemap",
-                    new XElement(xmlns + "loc", Uri.EscapeUriString(string.Format("{0}sitemap-set-{1}.xml", ConfigurationManager.AppSettings["CanonicalSiteBase"], count))),
+                    new XElement(xmlns + "loc", Uri.EscapeUriString(string.Format("{0}sitemap-set-{1}.xml.gz", ConfigurationManager.AppSettings["CanonicalSiteBase"], count))),
                     new XElement(xmlns + "lastmod", sitemapNodeIndex.OrderByDescending(x => x.LastModified).Select(x => x.LastModified).First().ToString("yyyy-MM-ddTHH:mm:sszzz", CultureInfo.InvariantCulture))));
 
                 var sitemapUrlset = new XElement(xmlns + "urlset");
