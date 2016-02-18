aa6e458d59aefcddd2f350e6e60e63c28af5db7b
michael-mason <cptshiner@gmail.com>
2016-02-18 14:24:07 +1100

    updated redirects
    - fixed bug in item redirect
    - added image redirect

---
+++ src/CollectionsOnline.RedirectWebSite/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CollectionsOnline.RedirectWebSite.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool Contains(this string input, string valueToCheck, StringComparison comparisonType)
+        {
+            if (string.IsNullOrWhiteSpace(input) || string.IsNullOrWhiteSpace(valueToCheck))
+                return false;
+
+            return input.IndexOf(valueToCheck, comparisonType) >= 0;
+        }
+    }
+}
--- src/CollectionsOnline.RedirectWebSite/Modules/RedirectModule.cs
+++ src/CollectionsOnline.RedirectWebSite/Modules/RedirectModule.cs
@@ -1,4 +1,6 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
+using CollectionsOnline.RedirectWebSite.Extensions;
 using Nancy;
 using Nancy.Responses;
 
@@ -19,9 +21,30 @@ namespace CollectionsOnline.RedirectWebSite.Modules
             // Sam the koala
             Get["/items/1550331/{name?}"] = parameters => new RedirectResponse(string.Format("{0}/specimens/1487596", canonicalSiteBase), RedirectResponse.RedirectType.Permanent);
 
-            Get["/items/{id:int}/{name*}"] = parameters => new RedirectResponse(string.Format("{0}/items/{1}", canonicalSiteBase, parameters.id), RedirectResponse.RedirectType.Permanent);
+            Get["/items/{id:int}/{name?}"] = parameters => new RedirectResponse(string.Format("{0}/items/{1}", canonicalSiteBase, parameters.id), RedirectResponse.RedirectType.Permanent);
 
-            Get["/themes/{id:int}/{name*}"] = parameters => new RedirectResponse(string.Format("{0}/articles/{1}", canonicalSiteBase, parameters.id), RedirectResponse.RedirectType.Permanent);
+            Get["/themes/{id:int}/{name?}"] = parameters => new RedirectResponse(string.Format("{0}/articles/{1}", canonicalSiteBase, parameters.id), RedirectResponse.RedirectType.Permanent);
+
+            //Images
+            Get["/itemimages/{id1:int}/{id2:int}/{filename}"] = parameters =>
+            {
+                int imageIrn = int.Parse(string.Format("{0}{1}", parameters.id1, parameters.id2));
+
+                var originalFilename = parameters.filename.Value as string;
+
+                string location = canonicalSiteBase;
+
+                if (originalFilename.Contains("large.jpg", StringComparison.OrdinalIgnoreCase))
+                    location = string.Format("{0}/content/media/{1}/{2}-large.jpg", canonicalSiteBase, imageIrn % 50, imageIrn);
+                else if (originalFilename.Contains("medium.jpg", StringComparison.OrdinalIgnoreCase))
+                    location = string.Format("{0}/content/media/{1}/{2}-medium.jpg", canonicalSiteBase, imageIrn % 50, imageIrn);
+                else if (originalFilename.Contains("small.jpg", StringComparison.OrdinalIgnoreCase))
+                    location = string.Format("{0}/content/media/{1}/{2}-small.jpg", canonicalSiteBase, imageIrn % 50, imageIrn);
+                else if (originalFilename.Contains("thumb", StringComparison.OrdinalIgnoreCase))
+                    location = string.Format("{0}/content/media/{1}/{2}-thumbnail.jpg", canonicalSiteBase, imageIrn % 50, imageIrn);
+
+                return new RedirectResponse(location, RedirectResponse.RedirectType.Permanent);
+            };
 
             Get["/{everythingelse*}"] = parameters => new RedirectResponse(canonicalSiteBase, RedirectResponse.RedirectType.Permanent);
         }
