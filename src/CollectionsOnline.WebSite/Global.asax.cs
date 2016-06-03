298a862373bb422a1af60e92644d63de7f6ee050
michael-mason <cptshiner@gmail.com>
2016-06-03 16:33:43 +1000

    fix for unwanted headers
    - fix for unwanted headers server and miniprofiler ids

--- src/CollectionsOnline.WebSite/Global.asax.cs
+++ src/CollectionsOnline.WebSite/Global.asax.cs
@@ -8,15 +8,10 @@ namespace CollectionsOnline.WebSite
 {
     public class Global : HttpApplication
     {
-        protected void Application_EndRequest(object sender, EventArgs e)
+        protected void Application_PreSendRequestHeaders(object sender, EventArgs e)
         {
-            // Remove headers that are not needed
-            var application = sender as HttpApplication;
-            if (application != null && application.Context != null)
-            {
-                application.Context.Response.Headers.Remove("Server");
-                application.Context.Response.Headers.Remove("X-MiniProfiler-Ids");
-            }
+            Response.Headers.Remove("Server");
+            Response.Headers.Remove("X-MiniProfiler-Ids");
         }
 
         protected void Application_Error(object sender, EventArgs e)
