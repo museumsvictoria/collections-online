2e0475f979c48903a6188e97bd193c5a09727129
michael-mason <cptshiner@gmail.com>
2020-05-05 15:40:36 +1000

    update legacy mapbox tileset
    - moved to newest mapbox api as well as updated legacy classic tileset to newer styles
    - moved geo-spatial stuff out of specimen details to avoid css being overridden
    - updated js packages

--- src/CollectionsOnline.WebSite/content/js/app/map.js
+++ src/CollectionsOnline.WebSite/content/js/app/map.js
@@ -9,13 +9,28 @@ module.exports = {
     this.Model = window.latlongsModel;
   },
   buildMap: function () {
-    if (this.Model !== undefined && this.Model.length != 0) {
-      L.mapbox.accessToken = 'pk.eyJ1IjoibW1hc29uIiwiYSI6IlFtOXZGSW8ifQ.mIKIjLOaVEDuLDQOM-ddzA';
-      var map = L.mapbox.map('map', 'mmason.c525da74').setView(this.Model[0], 8);
-      var marker = L.marker(this.Model[0]).addTo(map);
-      
-      map.scrollWheelZoom.disable();
-    } else {
+      if (this.Model !== undefined && this.Model.length != 0) {
+          // Mapbox expects long/lat instead of lat/long
+          var longLat = [this.Model[0][1], this.Model[0][0]];
+          mapboxgl.accessToken = 'pk.eyJ1IjoibW1hc29uIiwiYSI6IlFtOXZGSW8ifQ.mIKIjLOaVEDuLDQOM-ddzA';
+
+          var map = new mapboxgl.Map({
+              container: 'map',
+              style: 'mapbox://styles/mmason/ck9tfnhc90fv21inxraee3u6d',
+              center: longLat,
+              zoom: 8
+          });
+
+          var marker = new mapboxgl.Marker()
+              .setLngLat(longLat)
+              .addTo(map);
+
+          var nav = new mapboxgl.NavigationControl({
+              showCompass: false
+          });
+
+          map.addControl(nav, 'top-left');
+      } else {
       $('#map').hide();
     }
   },
