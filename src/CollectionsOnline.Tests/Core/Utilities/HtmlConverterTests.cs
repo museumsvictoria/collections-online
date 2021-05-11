4a2b233c923fa7200e704e147ebab53405528983
michael-mason <cptshiner@gmail.com>
2021-05-11 15:21:23 +1000

    upgraded and modified htmlsanitizer
    - html sanitizer now keeps child nodes to avoid removing potentially relevant markup
    - added new test for html converter

--- src/CollectionsOnline.Tests/Core/Utilities/HtmlConverterTests.cs
+++ src/CollectionsOnline.Tests/Core/Utilities/HtmlConverterTests.cs
@@ -19,5 +19,18 @@ namespace CollectionsOnline.Tests.Core.Utilities
             // Then
             result.ShouldBe("df Austriella corrugata (Deshayes, 1843)");
         }
+
+        [Fact]
+        public void HtmlSanitizer_WithMalformedHTML_ReturnsSanitizedHTML()
+        {
+            // Given
+            string result;
+            var html = @"<h3><font size=""3"">Peter Hunter OAM, Kodak Australasia Pty Ltd: Public Relations, 1961 - 1995</font></h3><h3><font size=""3"">Early career in England</font></h3>";
+            
+            // When
+            result = HtmlConverter.HtmlSanitizer(html).Html;
+            
+            result.ShouldBe("<h3>Peter Hunter OAM, Kodak Australasia Pty Ltd: Public Relations, 1961 - 1995</h3><h3>Early career in England</h3>");
+        }
     }
 }
