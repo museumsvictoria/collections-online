f90b6491e903c04f4a77a67e65f82142e8e94e71
michael-mason <cptshiner@gmail.com>
2021-05-12 12:15:22 +1000

    added image magick test
    - added image magick test due to its tendancy to break if binaries change

---
+++ src/CollectionsOnline.Tests/Import/Factories/ImageMediaTests .cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using CollectionsOnline.Tests.Resources;
+using ImageMagick;
+using Shouldly;
+using Xunit;
+
+namespace CollectionsOnline.Tests.Import.Factories
+{
+    public class ImageMediaTests : FileBasedTest
+    {
+        [Fact]
+        public void ImageMagick_Correctly_SavesThumbnailImage()
+        {
+            // Given
+            var outputFileName = $"{Files.OutputFolder}{Path.GetFileNameWithoutExtension(Files.Fish)}.jpg";
+
+            using (var image = new MagickImage())
+            {
+                image.Read(Files.Fish);
+
+                var profile = image.GetColorProfile();
+                image.Strip();
+                if (profile != null)
+                    image.SetProfile(profile);
+
+                image.Format = MagickFormat.Jpg;
+                image.Quality = 90;
+                image.FilterType = FilterType.Lanczos;
+                image.ColorSpace = ColorSpace.sRGB;
+                image.Resize(new MagickGeometry(250) {FillArea = true});
+                image.Crop(250, 250, Gravity.Center);
+                image.UnsharpMask(0.5, 0.5, 0.6, 0.025);
+
+                // When
+                image.Write(outputFileName);
+            }
+
+            // Then
+            File.Exists(outputFileName).ShouldBe(true);
+        }
+    }
+}
